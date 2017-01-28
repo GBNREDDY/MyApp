71c332e9e85601bfbf7992d95f6cac037f239280
trackfamily <myyoutestmail@gmail.com>
2017-01-28 12:16:45 +0530

    First commit

--- app/src/main/java/com/example/myaceraspire/myapp/MainActivity.java
+++ app/src/main/java/com/example/myaceraspire/myapp/MainActivity.java
@@ -1,17 +1,16 @@
 package com.example.myaceraspire.myapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,8 +22,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -32,6 +29,9 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ImageView iv=(ImageView) navigationView.getHeaderView(0).findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.ic_menu_slideshow);
+
         navigationView.setNavigationItemSelectedListener(this);
     }
 
